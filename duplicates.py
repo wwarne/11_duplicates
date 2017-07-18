e24bccd7a226eeca96441652e8d30c589bc462aa
Dmitry Plevkov <wwarne@gmail.com>
2017-07-18 17:14:32 +0300

    More clear and good-looking output for user.

--- duplicates.py
+++ duplicates.py
@@ -21,20 +21,26 @@ def print_duplicates(duplicate_list):
         print('Congratulations. You don\'t have duplicates in this directory.')
         return
     for files in duplicate_list:
-        print('Simular files {}'.format(files))
+        print('File `{}` is located at:'.format(os.path.basename(files[0])))
+        for file in files:
+            print(file)
+        print('----')
 
 
 def create_parser():
-    argument_parser = argparse.ArgumentParser()
-    argument_parser.add_argument('path', help='Path to a directory')
+    argument_parser = argparse.ArgumentParser(description='File duplicate finder')
+    argument_parser.add_argument('path', help='Search in this directory')
     return argument_parser
 
 
 if __name__ == '__main__':
     parser = create_parser()
     namespace = parser.parse_args()
-    print('Scanning files.')
+    print('Scanning files in a directory `{}`'.format(namespace.path), end='')
     all_files = build_filelist(namespace.path)
-    print('Finding duplicates.')
+    print(' [OK]')
+    print('Searching for duplicates', end='')
     all_duplicates = find_duplicates(all_files)
+    print(' [OK]')
+    print('-----List of duplicates-----')
     print_duplicates(all_duplicates)
